048c4289e9a841ce0978c8da2c48dd047ec69696
Anji Korra <anjichowhan009@gmail.com>
2025-09-19 16:55:48 +0530

    modified the function

--- main.java
+++ main.java
@@ -1,36 +1,66 @@
 import java.util.Scanner;
 
 public class main{
+    public static void calculator(){
+        Scanner sc=new Scanner(System.in);
+        int result=0;
+        while(true){
+            chooseOperation();
+            int num=sc.nextInt();
+
+            if(num==0 || num>4){
+                System.out.println("Invalid Input..existing from calculator: ");
+                break;
+            }   
+            System.out.println("Enter 1st Num: ");
+            int num1=sc.nextInt();
+            System.out.println("Enter 2nd Number: ");
+            int num2=sc.nextInt();  
+
+            if(num==1){
+                result=num1+num2;
+            }
+            else if(num==2){
+                result=num1-num2;
+            }
+            else if(num==3){
+                result=num1*num2;
+            }
+            else if(num==4){
+                if(num2!=0){
+                    result=num1/num2;
+                }
+                else{
+                    System.out.println("Error Division By Zero");
+                    continue;
+                }
+            }
+    
+        System.out.println("Result= "+ result);
+        System.out.println();
+    
+        System.out.println("Continue? Click S otherwise N:");
+        sc.nextLine(); 
+        String pass = sc.nextLine();
+
+        if (pass.equalsIgnoreCase("S")) { 
+            continue;
+        }else {
+            break;
+        }
+    }
+    }
+    public static void chooseOperation(){
+        System.out.println("1.Addition \n2.Subtraction\n3.Multiplication\n4.Division ");
+        System.out.println("Select your Operation: ");
+
+
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int operation,num1,num2;
-        int result=0;
-        System.out.println(" 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division");
-        System.out.println(" choose opertaion: ");
-        operation=sc.nextInt();
-        System.out.println("Enter Num1: ");
-        num1=sc.nextInt();
-        System.out.println("Enter Num12: ");
-        num2=sc.nextInt();
-
-        switch (operation) {
-            case 1:
-            result=num1+num2;
-                break;
-            case 2:
-            result=num1-num2;
-                break;
-            case 3:
-            result=num1*num2;
-                break;
-            case 4:
-            result=num1/num2;
-                break;
+        calculator();
         
-            default:
-            System.out.println("Invalid input.Try again ");
-                break;
-        }
-        System.out.println("Result = "+ result);
     }
 }
